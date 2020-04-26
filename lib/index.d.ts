70b9178698bf79ff69204e305bfe4833fb11aaa6
zhanphty <461632311@qq.com>
2020-04-26 11:50:31 +0800

    -del

--- lib/index.d.ts
+++
@@ -1,7 +0,0 @@
-import { PluginObject } from 'vue';
-import _VueEmojiBox from './VueEmojiBox.vue';
-import { renewHtml } from './utils/format';
-declare const VueEmojiBox: PluginObject<_VueEmojiBox>;
-export { VueEmojiBox, renewHtml };
-export default _VueEmojiBox;
-//# sourceMappingURL=index.d.ts.map
